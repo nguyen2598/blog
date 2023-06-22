cc92e6b3b10316c0ac957b5bf5469ee60cc9f4c1
dinhnguyen2598 <dinhnguyen250802@gmail.com>
2023-06-22 18:42:55 +0700

    i

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path=require('path')
+
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+
+const app = express()
+const port = 3000 // run ở cổng nào
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+
+// // template engine
+app.engine('hbs', handlebars.engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname, 'resources', 'views'));
+
+app.get('/', (req, res) => {// địnhh nghĩa router
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
